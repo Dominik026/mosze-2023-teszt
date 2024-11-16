4e584d672f79f0d0e350f537533deafbfa1b3050
Dominik026 <kaszadominik02@gmail.com>
2024-11-16 22:25:11 +0100

    Update main.cpp
    Hibák megjegyzés formájában

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; /*Helytelen változónév: NELEMENTS -> N_ELEMENTS*/ /*Hiányzó memóriadeallokálás: Az int *b = new int[N_ELEMENTS]; használatakor a delete[] b; sor hiányzik, amely felszabadítaná a lefoglalt memóriát. Javítás: A végén hozzáadjuk a delete[] b; sort a kód végére.*/
+    std::cout << '1-100 ertekek duplazasa' /*Hiányzó pontosvesszők: std::cout << '1-100 ertekek duplazasa'*/
+    for (int i = 0;) /*Hiányzó feltételek: Az első for ciklusban (int i = 0;) helytelen, mivel nem tartalmaz ciklusfeltételt és léptetést (i < N_ELEMENTS; i++).*/
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) /*A második for ciklusban a feltétel for (int i = 0; i; i++) hibás, mivel i; logikailag hamis, ezért a ciklus nem fut le. Helyes feltétel: (int i = 0; i < N_ELEMENTS; i++)*/
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" /*Hiányzó pontosvesszők: std::cout << "Ertek:"*/
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    std::cout << "Atlag szamitasa: " << std::endl; 
+    int atlag; /*Inicializálatlan változó: Az int atlag; változó nincs inicializálva, de később számításoknál használjuk. atlag értékét 0-ra kellene állítani kezdetben. Javítás: int atlag = 0;*/
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] /*Hiányzó pontosvesszők:   atlag += b[i]*/
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
